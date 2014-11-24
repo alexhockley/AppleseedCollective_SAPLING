0a22fc8e7a1b86f661eac992dc2aeb294d517fb8
Erica Pisani <pisani.erica@gmail.com>
2014-11-24 16:04:09 -0500

    Add Upcoming events side bar (barebones)

---
+++ includes/rightsidebar.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+	    <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <script type="text/javascript" src="<?php echo Helpers::BASE_URL_LOCAL?>/js/header.js"></script>
+	    <script type="text/javascript" src="<?php echo Helpers::BASE_URL_LOCAL?>/js/jquery-1.11.1.js"></script>
+	    <script type="text/javascript" src="<?php echo Helpers::BASE_URL_LOCAL?>/bootstrap/dist/js/bootstrap.min.js"></script>
+	    <link rel="stylesheet" href="<?php echo Helpers::BASE_URL_LOCAL?>/bootstrap/dist/css/bootstrap.min.css">
+	    <link rel="stylesheet" href="<?php echo Helpers::BASE_URL_LOCAL?>/css/style.css">
+		<link rel="stylesheet" href="<?php echo Helpers::BASE_URL_LOCAL?>/css/font-awesome.min.css">
+	</head>
+	<body>
+		<div id="right-side-bar" class="panel panel-default">
+			<div class="panel-body">Upcoming Events</div>
+
+		</div>
+	</body>
+</html>
