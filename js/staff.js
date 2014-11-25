d1abb08b8a9223041338727608eca2f307b7d713
Erica Pisani <pisani.erica@gmail.com>
2014-11-25 02:11:19 -0500

    Staff > Events Pending Approval/Delete Event sections with dummy data.

--- js/staff.js
+++ js/staff.js
@@ -1,10 +1,67 @@
+
 //When document is ready, aka, all elements are finished loading and created
 $(document).ready(function(){
+    testEvents();
     bindLoginEvents();
+    bindMenuButtons();
 });
+
+function testEvents() {
+	$("#test-view-button").click(function(){
+		$("#not-logged-in-view").hide();
+		$("#logged-in-view").show();
+
+		$("#staff-pending-events-head").show(); // Getting things started
+		$("#staff-create-gleaning-head").hide();
+		$("#staff-delete-gleaning-head").hide();
+
+		$("#staff-delete-events-body").hide();
+	});
+}
 
 function bindLoginEvents() {
 	$("#staff-login-button").click(function(){
 		alert("This works too!");
+	});
+}
+
+function bindMenuButtons() {
+	$("#staff-pending-events-button").click(function(){
+		$("#staff-pending-events-head").show();
+		$("#staff-create-gleaning-head").hide();
+		$("#staff-delete-gleaning-head").hide();
+
+		$("#staff-pending-events-button").attr("class", "active");
+		$("#staff-create-gleaning-button").attr("class", "");
+		$("#staff-delete-gleaning-button").attr("class", "");
+
+		$("#staff-pending-events-body").show();
+		$("#staff-delete-events-body").hide();
+	});
+
+	$("#staff-create-gleaning-button").click(function(){
+		$("#staff-pending-events-head").hide();
+		$("#staff-create-gleaning-head").show();
+		$("#staff-delete-gleaning-head").hide();
+
+		$("#staff-pending-events-button").attr("class", "");
+		$("#staff-create-gleaning-button").attr("class", "active");
+		$("#staff-delete-gleaning-button").attr("class", "");
+
+		$("#staff-pending-events-body").hide();
+		$("#staff-delete-events-body").hide();
+	});
+
+	$("#staff-delete-gleaning-button").click(function(){
+		$("#staff-pending-events-head").hide();
+		$("#staff-create-gleaning-head").hide();
+		$("#staff-delete-gleaning-head").show();
+
+		$("#staff-pending-events-button").attr("class", "");
+		$("#staff-create-gleaning-button").attr("class", "");
+		$("#staff-delete-gleaning-button").attr("class", "active");
+
+		$("#staff-pending-events-body").hide();
+		$("#staff-delete-events-body").show();
 	});
 }
--- staff.php
+++ staff.php
@@ -11,15 +11,106 @@ include_once('includes/rightsidebar.php');
 		</title>
 	</head>
 	<body>
-		<h2>Staff Members</h2>
-		<br /><br />
-		<p class="paragraph-body">
-			View gleaning events, register a gleaning, and cancel events.
-		</p>
-        <?php
-            if(!isset($token))
-                echo "<button id=\"staff-login-button\" class=\"btn btn-primary\" type=\"button\">Log In</button></li>";
-        ?>
+		<button id="test-view-button" class="btn" type="button">Test view</button><br />
+		<div id="not-logged-in-view">
+			<h2>Staff Members</h2>
+			<br /><br />
+			<p class="paragraph-body">
+				View gleaning events, register a gleaning, and cancel events.
+			</p>
+	        <?php
+	            if(!isset($token))
+	                echo "<button id=\"staff-login-button\" class=\"btn btn-primary\" type=\"button\">Log In</button></li>";
+	        ?>
+    	</div>
+
+    	<div id="logged-in-view">
+    		<ul id="staff-menu" class="nav nav-pills">
+		      <li id="staff-pending-events-button" role="presentation" class="active"><a href="#">Events Pending Approval</a></li>
+		      <li id="staff-create-gleaning-button" role="presentation"><a href="#">Create a Gleaning Event</a></li>
+		      <li id="staff-delete-gleaning-button" role="presentation"><a href="#">Delete a Gleaning Event</a></li>
+		    </ul>
+		    <br /><br />
+
+			<h2 id="staff-pending-events-head"><b>Events Pending Approval</b></h2>
+			<h2 id="staff-create-gleaning-head"><b>Create a Gleaning Event</b></h2>
+			<h2 id="staff-delete-gleaning-head"><b>Cancel a Gleaning Event</b></h2>
+			<br /><br />
+
+			<!-- Events Pending Approval-->
+    		<p id="staff-pending-events-body" class="paragraph-body">
+    			<i>Events that require staff approval before being displayed on the 'Upcoming Events' page
+    			 are listed below</i><br /><br />
+				<table class="table" id="staff-pending-events-table">
+					<thead>
+						<tr>
+							<th>Day</th>
+							<th>Location</th>
+							<th>Time</th>
+							<th>Duration</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>November 20, 2014</td>
+							<td>45 Water St., Guelph, ON</td>
+							<td>2:00 pm</td>
+							<td>2 hours</td>
+							<td><a>View Details</a></td>
+						</tr>
+						<tr>
+							<td>November 22, 2014</td>
+							<td>11 Guelph Line, Burlington, ON</td>
+							<td>11:30 am</td>
+							<td>3 hours</td>
+							<td><a>View Details</a></td>
+						</tr>
+					</tbody>
+				</table><br />
+    		</p>
+    		<!-- End of My Events Section -->
+
+    		<!-- Delete Event -->
+    		<p id="staff-delete-events-body" class="paragraph-body">
+    			<i>To delete an event, click 'View Details', and then 'Delete Event'</i><br /><br />
+				<table class="table" id="delete-event-table">
+					<thead>
+						<tr>
+							<th>Day</th>
+							<th>Location</th>
+							<th>Time</th>
+							<th>Duration</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>November 15, 2014</td>
+							<td>45 Smith St., Guelph, ON</td>
+							<td>2:30 pm</td>
+							<td>2 hours</td>
+							<td><a>View Details</a></td>
+						</tr>
+						<tr>
+							<td>November 22, 2014</td>
+							<td>1 Anders St., Hamilton, ON</td>
+							<td>10:00 am</td>
+							<td>3 hours</td>
+							<td><a>View Details</a></td>
+						</tr>
+						<tr>
+							<td>November 22, 2014</td>
+							<td>3484 Lakeshore Rd., Burlington, ON</td>
+							<td>11:00 am</td>
+							<td>1 hour</td>
+							<td><a>View Details</a></td>
+						</tr>
+					</tbody>
+				</table><br />
+			</p>
+    		<!-- End of Delete Event -->
+    	</div>
 	</body>
 </html>
 
